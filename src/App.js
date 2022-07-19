3cc7e62c7df4a246b87904e03201f00908d0ce00
Sara Heath <saramaheath@gmail.com>
2022-07-19 14:32:52 -0500

    add functionality to drawacard component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from "react"
-import axios from 'axios'
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DrawACard from "./DrawACard";
 
-const DECK_URL = 'http://deckofcardsapi.com/api/deck/new/'
+const DECK_URL = "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
+/**Api call to fetch new deck 
+ * 
+ * App --> DrawACard
+ */
 function App() {
-  const [deckId, setDeckId] = useState("Pick a deck..")
+  const [deckId, setDeckId] = useState("Pick a deck..");
   console.log("App", deckId);
 
-  useEffect (function fetchDeckIdWhenMounted () {
-    async function fetchDeckId () {
+  useEffect(function fetchDeckIdWhenMounted() {
+    async function fetchDeckId() {
       const deckId = await axios.get(DECK_URL);
-      setDeckId(deckId.data.deck_id)
+      setDeckId(deckId.data.deck_id);
     }
     fetchDeckId();
   }, []);
-
 
   return (
     <div className="App">
--- src/DrawACard.js
+++ src/DrawACard.js
@@ -1,9 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios";
 
+const CARD_URL = "http://deckofcardsapi.com/api/deck/";
 
+/**Shows card chosen with button to pick new card
+ *
+ * props:
+ * -deckId (str)
+ *
+ * App --> DrawACard
+ */
+function DrawACard({ deckId }) {
+  const [card, setCard] = useState({});
 
-function DrawACard({deckId}) {
-    const []
-    return (
-        <button onclick={}>Draw a Card!</button>
-    )
+  async function fetchCard() {
+    try {
+      const response = await axios.get(`${CARD_URL}${deckId}/draw/?count=1`);
+      setCard(response.data.cards[0]);
+      console.log("DrawACard", "remaining:", response.data.remaining);
+    } catch (err) {
+      setCard("");
+    }
+  }
+
+  return (
+    <div>
+      {!card && <p>"Error: no cards remaining"</p>}
+      {typeof card === "object" && (
+        <div>
+          {" "}
+          <p>
+            Value: {card.value} Suit: {card.suit}{" "}
+          </p>
+          <img src={card.image}></img>
+          <button onClick={fetchCard}>Draw a Card!</button>
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default DrawACard;
